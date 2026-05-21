3f38312b9bd58dda0122a283d391140df8227d03
RogerioSouza-f <souzarogerio33freitas@gmail.com>
2026-05-21 14:27:50 -0300

    Add Game data model for game attributes and parceling

---
+++ app/src/main/java/com/example/lojadejogos/model/Game.kt
@@ -0,0 +1,19 @@
+package com.example.lojadejogos.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Game(
+    val id: Int,
+    val title: String,
+    val developer: String,
+    val category: String,
+    val price: Double,
+    val rating: Double,
+    val description: String,
+    val bannerRes: Int,
+    val thumbnailRes: Int,
+    val isInstalled: Boolean = false,
+    val playTime: String? = null
+) : Parcelable
